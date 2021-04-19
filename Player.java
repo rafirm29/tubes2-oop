5a97044079fd060102fb13f8349f534ade18251a
rafirm29 <rafiray.munandar@gmail.com>
2021-04-19 20:47:43 +0700

    Added player methods and elmt adv description

--- Player.java
+++ Player.java
@@ -1,3 +1,51 @@
 public class Player {
-    
+    Engimon activeEngimon;
+    Inventory<Engimon> invEngimon;
+    Inventory<SkillItem> invSkill;
+    int x;
+    int y;
+
+    public Player(Engimon firstEngimon) {
+        this.activeEngimon = firstEngimon;
+        invEngimon = new Inventory<Engimon>();
+        invSkill = new Inventory<SkillItem>();
+        this.x = 0;
+        this.y = 1;
+    }
+
+    public Engimon getActiveEngimon() {
+
+    }
+
+    public void showInventoryEngimon() {
+
+    }
+
+    public void showInventorySkill() {
+
+    }
+
+    public void swapEngimon() {
+
+    }
+
+    public void useSkillItem() {
+
+    }
+
+    public void releaseItem(Engimon e) {
+
+    }
+
+    public void releaseItem(SkillItem si, int n) {
+
+    }
+
+    public void battle(Engimon enemy) {
+
+    }
+
+    public void breeding() {
+        
+    }
 }
