a2b2eb885d74db58230d93f990e7c62cb27afe5c
harithfakhiri <68526483+harithfakhiri@users.noreply.github.com>
2021-04-19 21:02:02 +0700

    Add files via upload

---
+++ Peta.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Peta {
+    private int baris;
+    private int kolom;
+    private ArrayList<WildEngimon> wildEngimons;
+    static Tile[][] peta;
+
+    public int getBaris() { return this.baris;}
+    public int getKolom() { return this.kolom;}
+    public void makePeta() {
+
+    }
+    public void printPeta() {
+
+    }
+
+    public void spawnEngimons() {
+
+    }
+}
