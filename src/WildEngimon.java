4cc3f8d1529304fc2c782abdf7fe3f8e6eee572f
harithfakhiri <68526483+harithfakhiri@users.noreply.github.com>
2021-04-24 15:59:02 +0700

    Update WildEngimon.java

--- src/WildEngimon.java
+++ src/WildEngimon.java
@@ -4,11 +4,12 @@ public class WildEngimon extends Engimon {
     private int y;
     private char symbol;
     
-    public WildEngimon(String name, SPECIES s, int x, int y, char symbol) {
+    public WildEngimon(String name, SPECIES s, int x, int y, int level) {
         super(name, s);
         this.x = x;
         this.y = y;
-        this.symbol = symbol;
+        this.symbol = Utils.speciesToString(this.getSpecies()).charAt(0);
+        this.setLevel(level);
     }
 
     public void setPosition(int x, int y) {
