61971929cdc08b3ba61814ffab20168ac9f6c6e7
ghallyy <ghallyarrahman12@gmail.com>
2021-04-26 01:09:27 +0700

    update

--- src/Game.java
+++ src/Game.java
@@ -1,71 +1,71 @@
-import java.awt.color.*;
 import java.awt.Color;
 import javax.swing.*;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.*;
 
-public class Game {
+public class Game{
     JFrame window;
     Container con;
-    JPanel titleNamePanel, startbutton, mainTextPanel;
-    JLabel titlenameLabel,startbuttonlabel;
-    Font titleFont = new Font("Times New Roman", Font.PLAIN,30);
-    Font normal = new Font("Times New Roman", Font.PLAIN, 20);
+    JPanel titleNamePanel, startButtonPanel, mainTextPanel;
+    JLabel titleNameLabel;
+    Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
+    Font normalFont = new Font("Times New Roman", Font.PLAIN, 30);
+    JButton startButton;
     JTextArea mainTextArea;
     TitleScreenHandler tsHandler = new TitleScreenHandler();
-
     public static void main(String[] args){
         new Game();
     }
+
     public Game(){
-        window = new JFrame();
-        window.setSize(800,600);
+        window =  new JFrame();
+        window.setSize(1920, 1080);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.getContentPane().setBackground(Color.black);
         window.setLayout(null);
         window.setVisible(true);
-        con =  window.getContentPane();
+        con = window.getContentPane();
 
         titleNamePanel =  new JPanel();
-        titleNamePanel.setBounds(100,100,600,150);
+        titleNamePanel.setBounds(100, 100, 600, 150);
         titleNamePanel.setBackground(Color.black);
-        titlenameLabel =  new JLabel("Willy Wangky and the Engimon Factory");
-        titlenameLabel.setForeground(Color.white);
-        titlenameLabel.setFont(titleFont);
+        titleNameLabel =  new JLabel("TUBES");
+        titleNameLabel.setFont(titleFont);
+        titleNameLabel.setForeground(Color.white);
+        titleNamePanel.add(titleNameLabel);
+        
 
-        startbutton =  new JPanel();
-        startbutton.setBounds(300,400,200,100);
-        startbutton.setBackground(Color.black);
-        startbuttonlabel = new JLabel("Start");
-        startbuttonlabel.setBackground(Color.black);
-        startbuttonlabel.setForeground(Color.white);
-        startbuttonlabel.setFont(normal);
-        startbuttonlabel.addActionListener(tsHandler);
+        startButtonPanel = new JPanel();
+        startButtonPanel.setBounds(300, 400, 200, 100);
+        startButtonPanel.setBackground(Color.black);
 
-        titleNamePanel.add(titlenameLabel);
-        startbutton.add(startbuttonlabel);
+        startButton = new JButton("START");
+        startButton.setFont(normalFont);
+        startButton.setBackground(Color.black);
+        startButton.setForeground(Color.white);
+        startButtonPanel.add(startButton);
+        startButton.addActionListener(tsHandler);
 
         con.add(titleNamePanel);
-        con.add(startbutton);
+        con.add(startButtonPanel);
     }
 
     public void inGame(){
-        titleNamePanel.setVisible(false);
-        startbutton.setVisible(false);
 
-        mainTextPanel = new JPanel();
-        mainTextPanel.setBounds(100,100,600,250);
+        startButtonPanel.setVisible(false);
+        titleNamePanel.setVisible(false);
+        mainTextPanel =  new JPanel();
+        mainTextPanel.setBounds(100, 100, 600, 250);
         mainTextPanel.setBackground(Color.blue);
         con.add(mainTextPanel);
-
-        mainTextArea  =  new JTextArea();
+        mainTextArea = new JTextArea("Petanya nanti diload disini");
         mainTextArea.setBounds(100, 100, 600, 250);
         mainTextArea.setBackground(Color.black);
         mainTextArea.setForeground(Color.white);
-        mainTextArea.setFont(normal);
+        mainTextArea.setFont(normalFont);
         mainTextArea.setLineWrap(true);
         mainTextPanel.add(mainTextArea);
     }
